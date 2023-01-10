763f1379dd4029bfe1a278313efd13fe9bdff5f6
Maximiliam999 <maximanuel777@gmail.com>
2023-01-10 16:28:29 +0000

    Add switch for machine

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+ 
+
+ 
+
+let man;
+function machineTurn() {
+let machine = Math.floor(Math.random() * 5);
+
+switch(choice){
+    case 0: machine="rock";
+    break;
+    case 1: machine="paper";
+    break;
+    case 2: machine="scissors";
+    break;
+    case 3: machine="lizard";
+    break;
+    case 4: machine="spock";
+    break;
+}
+
+}
