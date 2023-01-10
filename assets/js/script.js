7bca2de3a066776147a0d991435670f2b836b6d3
Maximiliam999 <maximanuel777@gmail.com>
2023-01-10 18:36:05 +0000

    Add game funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
- 
+const playerText = document.getElementById("playerText");
+const machinceText= document.getElementsById("machineText");
+const resultText= document.getElementById("resultText");
+const scoreBoard= document.getElementById("score-board")
+const btnChoice= document.getElementsByClassName("Btn-choice");
+let manScore= 0; 
+let machineScore= 0;
 
- 
+function manChoice() {
+    
+}
 
-let man;
-function machineTurn() {
-let machine = Math.floor(Math.random() * 5);
+
+function machineChoice() {
+let machine = Math.floor(Math.random() * 5) + 1;
 
 switch(choice){
     case 0: machine="rock";
@@ -18,5 +26,54 @@ switch(choice){
     case 4: machine="spock";
     break;
 }
+
+}
+
+function checkResult(){
+    
+if (manChoise === machinceChoise) {
+    return "It\"s a tie, the CPU also chose" + machinceChoise + ".";
+} else if (manChoise === "rock"){
+    if (machinceChoise === "paper" || "spock"){
+        return "You lose";
+        cpuScore++;
+    } else if (machinceChoise === "scissors" || "lizard"){
+        return "You win";
+        userScore++;
+    }
+    } else if(manChoice === "paper"){
+        if(machineChoice === "scissors"|| "lizard"){
+            return "You lose";
+            machineScore++;
+        } else if(machineChoice === "rock" || "spock"){
+            return "You win";
+            manScore++;
+        } 
+        
+    } else if(manChoice === "scissors"){
+        if(machineChoice === "rock" || "spock"){
+            return "You lose";
+            machineScore++ 
+        } else if(machineChoice === "paper" || "lizard"){
+            return "You win";
+            manScore++
+        }
+    } else if(manChoice === "lizard"){
+        if(machineChoice === "rock" || "scissor"){
+            return "You lose";
+            manScore++;
+        } else if (machineChoice === "paper" || "spock"){
+            return "You win";
+            manScore++
+        }
+    } else if(manChoice === "spock"){
+        if(machineChoice === "paper" || "lizard"){
+            return "You lose";
+            machineScore++
+        } else if (machineChoice === "rock" || "scissor"){
+            return "You win";
+            manScore++
+        }
+    }
 
 }
